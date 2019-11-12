b5720b6ba261b85cca1ee5a163ffe8381b66aec4
aabc88 <aabc88@nate.com>
2019-11-12 15:05:06 +0900

    Variable type

---
+++ Variable/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 100;
+		System.out.println("int i = "+i);
+		
+		char c = 'a';
+		System.out.println("char c = "+c);
+		
+		double d = 10.123;
+		System.out.println("double d = "+d);
+		
+		float f = 10.123f;
+		System.out.println("float f = "+f);
+		
+		boolean b = false;
+		System.out.println("boolean b = "+b);
+		
+		String s = "Hello world";
+		System.out.println("String s = "+s);
+	}
+
+}
